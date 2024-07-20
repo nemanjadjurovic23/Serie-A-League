45556e69c2da7621ce64c5e15ac13cc91dd311d9
nemanjadjurovic23 <nemanjadjurovic640@gmail.com>
2024-07-20 11:03:49 +0200

    * Optimized imports

--- app/Console/Commands/GetFootballTeams.php
+++ app/Console/Commands/GetFootballTeams.php
@@ -5,7 +5,6 @@ namespace App\Console\Commands;
 use App\Models\TeamsModel;
 use App\Services\FootballApiServices;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Http;
 
 class GetFootballTeams extends Command
 {
--- app/Http/Controllers/FootballController.php
+++ app/Http/Controllers/FootballController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\StandingsModel;
 use App\Models\TeamsModel;
-use App\Services\FootballApiServices;
-use Illuminate\Http\Request;
 
 class FootballController extends Controller
 {
